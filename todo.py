665429a67af474d415e39f3a67f8c8b7b96351f2
Tro48 <139925929+Tro48@users.noreply.github.com>
2023-07-26 00:51:01 +0300

    Создал скрипт с добавлением задач (#1)
    * Создал скрипт с добавлением задач
    
    * Сделал необходимые правки
    Добавил проверку на пустую строку
    
    Думаю найти способ как добавлять задание без ковычек
    и как переходить к вводу команды пошагово.К примеру:
    вначале заход в файл python todo.py, потом вторым шагом ввод комманды.
    
    * поправил проверку через elif

---
+++ todo.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+if os.path.isfile("list_of_entries.txt"):
+    entries = open('list_of_entries.txt', encoding='utf-8')
+    print('Весь список дел:' "\n" + entries.read())
+    entries.close()
+else:
+    print("Пока что нет записей...")
+
+if len(sys.argv) == 1:
+    print("Это Help. Что бы добавить задачу введите add 'Текст задачи в ковычках'.")
+    sys.exit(1)
+elif len(sys.argv) < 3:  # Проверка на пустую строку после add
+    print("Ошибка! Вы не ввели задание!")
+    sys.exit(1)
+
+
+else:
+    commands = sys.argv[1]
+
+    if commands == "add":
+        entries_text = sys.argv[2]
+        entries_add = open('list_of_entries.txt', 'a+', -1, 'utf-8')
+        entries_add.writelines(entries_text + "\n")
+        entries_add.close()
+        entries = open('list_of_entries.txt', encoding='utf-8')
+        print("Новое задание добавлено в список!")
+        print('Новый список дел:' "\n" + entries.read())
+        entries.close()
+        sys.exit(1)
+
+    else:
+        print("такой комманды нет!")
+        entries = open('list_of_entries.txt', encoding='utf-8')
+        print('Весь список дел:' "\n" + entries.read())
+        entries.close()
