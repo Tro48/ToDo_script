9e132e438abb77023bdf45149c0081e24dd9cba9
Tro48 <trofimander48@gmail.com>
2023-07-28 21:43:17 +0300

    добавил функцию нумерации строк

--- todo.py
+++ todo.py
@@ -2,27 +2,59 @@ import sys
 import os
 
 
-def add_line(text):
-    with open('list_of_entries.txt', 'a+', -1, 'utf-8') as entries_add:
-        entries_add.writelines(text + "\n")
-        entries_add.close()
-        print("Новое задание добавлено в список!")
-
-
 def write_entries():
     if os.path.isfile("list_of_entries.txt"):
         with open('list_of_entries.txt', encoding='utf-8') as entries:
             print('Весь список дел:' "\n" + entries.read())
+
     else:
         print("Пока что нет записей...")
 
 
+def add_line(text):
+
+    with open('list_of_entries.txt', 'a+', -1, 'utf-8') as entries_add:
+        number = number_lines()
+        n = str(number[-1] + 1)
+        entries_add.writelines(n + "." + text + "\n")
+
+
+        print("Новое задание добавлено в список!")
+
+
+def edit_line():
+    pass
+
+
+def remove_line():
+    pass
+
+
+def number_lines():
+    entries_list = []
+    list_index = []
+    if os.path.isfile("list_of_entries.txt"):
+        if os.stat('list_of_entries.txt').st_size != 0:
+            with open('list_of_entries.txt', encoding='utf-8') as lines:
+                for line in lines:
+                    current_line = line[:-1]
+                    entries_list.append(current_line)
+                for i in entries_list:
+                    index = entries_list.index(i)
+                    index += 1
+                    list_index.append(index)
+        else:
+            list_index = [0]
+
+        return list_index
+
+
 if len(sys.argv) == 1:
     write_entries()
-    sys.exit(1)
+    sys.exit()
 elif len(sys.argv) < 3:
     print("Ошибка! Вы не ввели задание!")
-    sys.exit(1)
+    sys.exit()
 
 
 else:
@@ -31,7 +63,7 @@ else:
     if commands == "add":
         entries_text = sys.argv[2]
         add_line(entries_text)
-        sys.exit(2)
+        sys.exit()
     else:
         print("такой комманды нет!")
         print("Что бы добавить задачу введите add 'Текст задачи в ковычках'.")
